5289066fbf520cee8d0613d25f59915df7d54fe2
Chris <chris@Chriss-MacBook-Pro.local>
2019-05-31 17:04:07 -0600

    configure db context

--- cc-alpha-api/Startup.cs
+++ cc-alpha-api/Startup.cs
@@ -27,13 +27,13 @@ namespace cc_alpha_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                services.AddDbContext<TestContext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("Server=tcp:alpha-cc.database.windows.net,1433;Initial Catalog=alpha-cc-db;Persist Security Info=False;User ID=alphaCCDbUser;Password=azCC1!User;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")));
+            // // Use SQL Database if in Azure, otherwise, use SQLite
+            // if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            //     services.AddDbContext<TestContext>(options =>
+            //             options.UseSqlServer(Configuration.GetConnectionString("Server=tcp:alpha-cc.database.windows.net,1433;Initial Catalog=alpha-cc-db;Persist Security Info=False;User ID=alphaCCDbUser;Password=azCC1!User;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")));
 
-            // Automatically perform database migration
-            services.BuildServiceProvider().GetService<TestContext>().Database.Migrate();
+            // // Automatically perform database migration
+            // services.BuildServiceProvider().GetService<TestContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
