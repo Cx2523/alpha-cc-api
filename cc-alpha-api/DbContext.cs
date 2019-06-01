da22c74b538772c184cb008ee843ad3717337b63
Chris <chris@Chriss-MacBook-Pro.local>
2019-06-01 07:00:10 -0600

    configure db context

--- cc-alpha-api/DbContext.cs
+++ cc-alpha-api/DbContext.cs
@@ -4,13 +4,14 @@ namespace cc_alpha_api
 {
     public class TestContext : DbContext
     {
-        public DbSet<Test> Test { get; set; }
+        public DbSet<Test> Tests { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 "Server=tcp:alpha-cc.database.windows.net,1433;Initial Catalog=alpha-cc-db;Persist Security Info=False;User ID=alphaCCDbUser;Password=azCC1!User;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
             );
+            base.OnConfiguring(optionsBuilder);
         }
     }
 
