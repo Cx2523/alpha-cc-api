a5551ed9e86b955d9e2f3e8c6b3e3cb43d283c17
Chris <chris@Chriss-MacBook-Pro.local>
2019-06-01 20:29:39 -0600

    add db migrations

--- cc-alpha-api/DbContext.cs
+++ cc-alpha-api/DbContext.cs
@@ -9,7 +9,7 @@ namespace cc_alpha_api
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                "Server=tcp:alpha-cc.database.windows.net,1433;Initial Catalog=alpha-cc-db;Persist Security Info=False;User ID=alpha-cc-admin;Password=azCCpw1!;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                "Server=tcp:alpha-cc.database.windows.net,1433;Initial Catalog=alpha-cc-db;Persist Security Info=False;User ID=TestUser;Password=azCCpw1!User;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
             );
             base.OnConfiguring(optionsBuilder);
         }
